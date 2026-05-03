450c69b38546a118d0e13a58afb5edde5233b218
Tom Pearson <tompearson@Toms-MacBook-Air.local>
2026-05-03 02:23:20 +0100

    Force light color scheme to fix dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${outfit.variable} ${dmSans.variable} h-full antialiased`}
+      style={{ colorScheme: "light" }}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
